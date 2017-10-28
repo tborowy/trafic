2800ed2943ded757b1c548421ac81c89fb8608c1
Magdalena Pal <magdalena.pal90@gmail.com>
2017-10-28 20:00:50 +0200

    Create controller.js, add request post

---
+++ front/controller.js
@@ -0,0 +1,21 @@
+var app = angular.module('hackatonApp', []);
+
+app.controller('adressCtrl', function($scope) {
+
+	var data = {
+	};
+	
+	$http.post('/api/file', data, {
+			headers: {
+					'Content-Type': 'application/json'
+			}
+	})
+	.success(function(data, status, headers, config) {
+			console.log("ok");
+			$scope.test = data;
+
+	}).error(function() {
+     console.log("error");
+	});   
+   
+});

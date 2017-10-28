4979b4e7733c36810b469f801f65f12aef55bb27
Magdalena Pal <magdalena.pal90@gmail.com>
2017-10-28 23:36:53 +0200

    Change logic in service.js

--- front/controller.js
+++ front/controller.js
@@ -1,11 +1,7 @@
  angular.module('myApp').controller('adressCtrl',
 	['adressService', '$scope', function (adressService, $scope) {
-		$scope.createProduct = function() {
-			var state = {
-				 state: "start"
-			};
-			adressService.create(state, function(data) {
-			  console.log("good");
-			});
-	};
+
+		adressService.upload().then(function (result) {
+      $scope.test = result;
+		});
 }])
